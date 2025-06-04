2c43602d35d44f41cabcca97d0223cfc4f9dfd8b
Pankaj <pankaj.kushwaha1801@gmail.com>
2025-06-04 11:27:03 +0530

    Migration Issue Solve

--- src/datasources/postgres.datasource.ts
+++ src/datasources/postgres.datasource.ts
@@ -1,17 +1,23 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 // Local Server
 const config = {
   name: 'postgres',
   connector: 'postgresql',
-  url: process.env.DATABASE_URL ?? '',
   host: process.env.DB_HOST ?? 'localhost',
   port: +(process.env.DB_PORT ?? 5432),
   user: process.env.DB_USER ?? '',
   password: process.env.DB_PASSWORD ?? '',
   database: process.env.DB_DATABASE ?? '',
+  connectTimeout: 10000,
 };
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('DB Config:', config);
+}
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
