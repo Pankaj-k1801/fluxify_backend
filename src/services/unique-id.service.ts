1fcfa1e06454661bc6322b6dae99e03148020071
Pankaj <pankaj.kushwaha1801@gmail.com>
2025-06-12 15:36:19 +0530

    Generating Methods to Increment Branch Unit Intitlized Ids

--- src/services/unique-id.service.ts
+++ src/services/unique-id.service.ts
@@ -1,6 +1,6 @@
 import {BindingScope, injectable} from '@loopback/core';
 import {repository} from '@loopback/repository';
-import {OrganizationRepository, SetupRepository} from '../repositories';
+import {BranchUnitRepository, OrganizationRepository, SetupRepository} from '../repositories';
 
 @injectable({scope: BindingScope.SINGLETON})
 export class UniqueIdService {
@@ -10,6 +10,9 @@ export class UniqueIdService {
 
     @repository(OrganizationRepository)
     private organizationRepository: OrganizationRepository,
+
+    @repository(BranchUnitRepository)
+    private branchUnitRepository: BranchUnitRepository,
 
   ) { }
 
@@ -70,6 +73,186 @@ export class UniqueIdService {
 
     // 4. Return current value before incrementing
     return fullBranchId;
+  }
+
+  /* Generating Next ids in Branch Unit Table */
+
+  /* Generating Next Brand Id */
+  async generateNextBrandId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'brand-';
+    const currentId = parseInt(branchUnit.nextBrandId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    // update back
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextBrandId: newId,
+    });
+
+    return branchUnit.nextBrandId; // return the one used before increment
+  }
+
+  /* Generating Next ProductType Id */
+  async generateNextProductTypeId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'productType-';
+    const currentId = parseInt(branchUnit.nextproductTypeId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    // update back
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextproductTypeId: newId,
+    });
+
+    return branchUnit.nextproductTypeId; // return the one used before increment
+  }
+
+  /* Generating Next Product Id */
+  async generateNextProductId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'product-';
+    const currentId = parseInt(branchUnit.nextproductId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextproductId: newId,
+    });
+
+    return branchUnit.nextproductId;
+  }
+
+  /* Generating Next ProductIUnit Id */
+  async generateNextProductUnitId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'productUnit-';
+    const currentId = parseInt(branchUnit.nextproductUnitId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextproductUnitId: newId,
+    });
+
+    return branchUnit.nextproductUnitId;
+  }
+
+  /* Generating Next Discount Id */
+  async generateNextDiscountId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'discount-';
+    const currentId = parseInt(branchUnit.nextDiscountId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextDiscountId: newId,
+    });
+
+    return branchUnit.nextDiscountId;
+  }
+
+  /* Generating Next PaymentInfo Id */
+  async generateNextPaymentInfoId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'paymentInfo-';
+    const currentId = parseInt(branchUnit.nextpaymentInfoId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextpaymentInfoId: newId,
+    });
+
+    return branchUnit.nextpaymentInfoId;
+  }
+
+  /* Generating Next PurchaseBill Id */
+  async generateNextPurchasedBillId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'purchasedBill-';
+    const currentId = parseInt(branchUnit.nextpurchasedBillId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextpurchasedBillId: newId,
+    });
+
+    return branchUnit.nextpurchasedBillId;
+  }
+
+  /* Generating Next SellBill Id */
+  async generateNextSellBillId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'sellBill-';
+    const currentId = parseInt(branchUnit.nextsellBillId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextsellBillId: newId,
+    });
+
+    return branchUnit.nextsellBillId;
+  }
+
+  /* Generating Next Supplier Id */
+  async generateNextSupplierId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'supplier-';
+    const currentId = parseInt(branchUnit.nextsupplierId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextsupplierId: newId,
+    });
+
+    return branchUnit.nextsupplierId;
+  }
+
+  /* Generating Next Customer Id */
+  async generateNextCustomerId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'customer-';
+    const currentId = parseInt(branchUnit.nextcustomerId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextcustomerId: newId,
+    });
+
+    return branchUnit.nextcustomerId;
+  }
+
+  /* Generating Next Staff Id */
+  async generateNextStaffId(orgId: string): Promise<string> {
+    const branchUnit = await this.branchUnitRepository.findOne({where: {orgId}});
+    if (!branchUnit) throw new Error('BranchUnit configuration not found');
+
+    const prefix = 'staff-';
+    const currentId = parseInt(branchUnit.nextStaffId.replace(prefix, ''), 10);
+    const newId = `${prefix}${currentId + 1}`;
+
+    await this.branchUnitRepository.updateById(branchUnit.id, {
+      nextStaffId: newId,
+    });
+
+    return branchUnit.nextStaffId;
   }
 
 }
